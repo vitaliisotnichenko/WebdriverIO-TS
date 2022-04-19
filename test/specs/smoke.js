92afbc6d97bc22a7a3021a91ce7e5e39cb2136a3
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-04-19 13:26:22 +0200

    First test

---
+++ test/specs/smoke.js
@@ -0,0 +1,15 @@
+const SecurePage = require("../pageobjects/secure.page");
+describe('Login app', () => {
+    it('should login with valid credentials', async () => {
+        browser.url('/login');
+        const inputUserName = $('#username');
+        const inputPassword = $('#password');
+        const btnSubmit = $('button');
+        await inputUserName.setValue('tomsmith');
+        await inputPassword.setValue('SuperSecretPassword');
+        await btnSubmit.click();
+        expect('#flash').toBeExisting();
+        expect('#flash').toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+})
