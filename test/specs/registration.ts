56e77011da5ce4a11b1119f553b1ee8cb05eba2e
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-04-25 10:58:13 +0200

    Update registration test

--- test/specs/registration.ts
+++ test/specs/registration.ts
@@ -6,28 +6,25 @@ describe('User', function () {
         const firstName = faker.name.firstName();
         const lastName = faker.name.lastName();
         const address = faker.address.streetAddress();
-        await  $('#email_create').waitForDisplayed({ timeout: 5000 });
+        $('#email_create').clearValue();
         $('#email_create').setValue(`test${new Date().getTime()}@test.com`);
-        await browser.pause(10000)
+        await browser.pause(5000);
         $('#SubmitCreate').click();
-        $('#customer_firstname').waitForDisplayed({ timeout: 5000 });
+        await browser.pause(2000);
         $('#customer_firstname').setValue(firstName);
         $('#customer_lastname').setValue(lastName);
-        await browser.pause(2000)
-        $('input[type="password"]').setValue('123456');
+        await browser.pause(2000);
+        $('input[type="password"]').setValue('123456Qq@');
         $('#firstname').setValue(firstName);
         $('#lastname').setValue(lastName);
-        await browser.pause(2000)
         $('#address1').setValue(address);
         $('#city').setValue('New York');
-        await browser.pause(2000)
+        $('#id_state').clearValue();
         $('#id_state').selectByVisibleText('Alaska');
-        await browser.pause(2000)
         $('#postcode').setValue('93995');
         $('#phone_mobile').setValue('1-264-963-3754 801');
-        await browser.pause(5000)
+        await browser.pause(2000);
         $('#submitAccount').click();
-        await browser.pause(5000)
         await expect($('.account')).toBeDisplayed();
     })
 })
